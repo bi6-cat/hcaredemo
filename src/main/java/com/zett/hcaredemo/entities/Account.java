c8feae78050b17739e22ad5ea1410e6040d6bbc0
vilua0202 <vilua2k4@gmail.com>
2024-10-19 02:04:21 +0700

    feature: add entities

--- src/main/java/com/zett/hcaredemo/entities/Account.java
+++ src/main/java/com/zett/hcaredemo/entities/Account.java
@@ -5,21 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "medical_records")
-public class MedicalRecord {
+@Table(name = "accounts")
+public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    private LocalDate date;
-    private String diagnosis;
-    private String treatment;
-    private String prescription;
-    private String notes;
+    private String username;
+    private String password;
+    private boolean isActivated;
+    private boolean isLockPermanent;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
 }
--- src/main/java/com/zett/hcaredemo/entities/Appointment.java
+++ src/main/java/com/zett/hcaredemo/entities/Appointment.java
@@ -1,5 +1,25 @@
 package com.zett.hcaredemo.entities;
 
-public class Appointment {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "appointments")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private LocalDate date;
+    private LocalTime time;
+    private String status;
+    private String notes;
 }
