5b92794ca3f5ddf4e8d984e0f6f081c5078829ef
vilua0202 <vilua2k4@gmail.com>
2024-11-08 02:54:48 +0700

    fix: role dto

--- src/main/java/com/zett/hcaredemo/dto/auth/RoleDTO.java
+++ src/main/java/com/zett/hcaredemo/dto/auth/RoleDTO.java
@@ -1,10 +1,17 @@
 package com.zett.hcaredemo.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoleDTO {
+    private UUID id;
     @NotBlank(message = "Role name is required")
     private String name;
 }
